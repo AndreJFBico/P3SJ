ced873c7cb35c776f25f64ca9f6397227d413a08
ScientX <tori789@gmail.com>
2014-02-23 00:38:59 +0000

    Iniciated

---
+++ projects/Assignment0/src/Interface.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Includes.h"
+#include "Manager.h"
+
+class Interface
+{
+	int WindowHandle;
+	bool cameraMode;
+
+	Interface() {};
+	Interface(Interface const&);
+
+public:
+	static Interface& getInstance()
+	{
+		static Interface instance;
+		return instance;
+	}
+
+	void initInterface(int val);
+
+	void setWindowHandle(int val);
+	int getWindowHandle();
+
+	void displayWindow();
+
+	void onMouse(int button, int state, int x, int y);
+	void mouseWheel(int button, int dir, int x, int y);
+	void KeyboardFunc(unsigned char key, int x, int y);
+	void specialkeyPressed(int key, int x, int y);
+	void passiveMotionFunc(int x, int y);
+};
---
+++ projects/Assignment0/src/Manager.cpp
@@ -0,0 +1,74 @@
+#include "Manager.h"
+
+void Manager::initManager()
+{
+	Objs = new std::vector<Drawable*>;
+	camera = new Camera();
+}
+
+void Manager::initScene()
+{
+	camera->setPerspective(tFOVY, WINDOW_WIDTH / WINDOW_HEIGHT, tNEAR, tFAR);
+	camera->setCenter(glm::vec3(0.0, 0.0, -5.0f));
+	camera->updateCamera();
+	ShaderProgram* sh = createShaderProgram("..\\shaders\\vertex_shader.glsl", "..\\shaders\\fragment_shader.glsl");
+
+	Texture* tex = new Texture2D();
+	PieceReader::getInstance().readObject("..\\objects\\monkey.obj");
+	tex->load("..\\textures\\monkey.psd");
+	Piece *p = new Piece(PieceReader::getInstance().getVertices(), PieceReader::getInstance().getIndices(), sh, tex, 0);
+	Objs->push_back(p);
+	PieceReader::getInstance().clearAll();
+}
+
+void Manager::draw()
+{
+	for (std::vector<Drawable*>::iterator it = Objs->begin(); it != Objs->end(); ++it)
+	{
+		(*it)->draw(camera->getViewMatrix(), camera->getProjectionMatrix(), camera->computeCameraCenter());
+	}
+}
+
+void Manager::updateCameraRotation(float x, float y)
+{
+	camera->setRotationAngleY(camera->getRotationAngleY() + (x - camera->getLast_mx()));
+	camera->setRotationAngleX(camera->getRotationAngleX() + (y - camera->getLast_my()));
+	camera->setLast_mx(x);
+	camera->setLast_my(y);
+	camera->updateCamera();
+}
+
+void Manager::updateCameraPosition(float x, float y)
+{
+	camera->setCenter(glm::vec3(camera->getCenter().x + ((x - camera->getLast_mx()) / -camera->getCenter().z), camera->getCenter().y + ((y - camera->getLast_my()) / -camera->getCenter().z), camera->getCenter().z));
+}
+
+void Manager::updateCameraZoom(int amount)
+{
+	if (amount > 0)
+		camera->setCenter(glm::vec3(camera->getCenter().x, camera->getCenter().y, camera->getCenter().z + 1));
+	else camera->setCenter(glm::vec3(camera->getCenter().x, camera->getCenter().y, camera->getCenter().z - 1));
+}
+
+void Manager::updateLastMXY(float x, float y)
+{
+	camera->setLast_mx(x);
+	camera->setLast_my(y);
+	camera->updateCamera();
+}
+
+ShaderProgram* Manager::createShaderProgram(std::string vertexShaderPath, std::string fragmentShaderPath)
+{
+	ShaderProgram *shProg = new ShaderProgram();
+
+	shProg->addVertexShader(vertexShaderPath.c_str());
+	shProg->addFragmentShader(fragmentShaderPath.c_str());
+
+	glBindAttribLocation(shProg->getProgram(), VERTICES, "in_Position");
+	glBindAttribLocation(shProg->getProgram(), COLORS, "in_Color");
+	glBindAttribLocation(shProg->getProgram(), NORMALS, "in_Normal");
+	glBindAttribLocation(shProg->getProgram(), TEXTURE, "in_Texture");
+	shProg->link();
+
+	return shProg;
+}
