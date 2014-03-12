c29392fde31ed4a54c7dcdb66c83fc9fffc399ed
JoaoLReis <jype_13@hotmail.com>
2014-03-12 22:24:30 +0000

    shader commented

--- projects/Assignment0/shaders/SphereM_fragment_shader.glsl
+++ projects/Assignment0/shaders/SphereM_fragment_shader.glsl
@@ -36,21 +36,27 @@ void main(void)
 
 	vec3 V = vec3(ex_Vertex);
 	vec3 N = normalize(ex_Normal);
-	
-	vec3 Lpos = vec3(ViewMatrix * vec4(LightPosition, 1.0f));//luz em eyespace
+
+	//luz em eyespace
+	vec3 Lpos = vec3(ViewMatrix * vec4(LightPosition, 1.0f));
 	vec3 L = Lpos - V;
 	float Ldist = length(L);
 	L = normalize(L);
-	vec3 E = normalize(-V);//eyespace a posição da camera é 0,0,0
+
+	//eyespace a posição da camera é 0,0,0
+	vec3 E = normalize(-V);
 	vec3 H = normalize(L + E);
-	vec3 R = reflect(-L,N);//eyespace a posição da camera é 0,0,0
 	
-	vec3 ambient = AmbientLightColor * MaterialAmbientColor;//chega ao objecto de igual forma
+	//eyespace a posição da camera é 0,0,0
+	vec3 R = reflect(-L,N);
+	
+	//chega ao objecto de igual forma
+	vec3 ambient = AmbientLightColor * MaterialAmbientColor;
 	
 	float NdotL = clamp(dot(N,L),0.0,1.0);
 	vec3 diffuse = LightDiffuseColor * MaterialDiffuseColor * NdotL;
-	//reflecção especular
 	
+	//reflecção especular
 	vec3 specular = vec3(0.0);
 	if(NdotL > 0.0) {
 		float NdotH = clamp(dot(R,H),0.0,1.0);
